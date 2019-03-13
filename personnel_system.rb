d14031d94b0f3766885668404bbf7778c01c51c8
DrewJCrawford <drew@woodenstoneconsulting.com>
2019-03-13 09:05:46 -0400

    menu.1.0
    First take at menu

--- personnel_system.rb
+++ personnel_system.rb
@@ -1,10 +1,49 @@
 require_relative "employee_class"
 
+def menu
+  puts "Employee Database 1000
+  1. Add New Employee
+  2. Add Location
+  3. Change Employee Location
+  4. List Locations
+  5. List Employees
+  6. Remove Employee"
+  case gets.chomp.to_i
+  when 1
+    puts "Create new employee:"
+    new_emp
+  when 2
+    puts "Create new location:"
+    new_location
+  when 3
+    puts "Change employee location:"
+    change_location
+  when 4
+     puts "Biz location list:"
+    list_location
+  when 5
+    puts "List of Employees:"
+    list_emp
+  when 6
+    puts "Your time here has ended:"
+    remove_employee
+  else
+    puts "Invalid entry, try again"
+    menu
+  end
+
+
+
+end
+
 def add_employee
 
 Employee.new(name, role, location)
 end
 
+
+
+end
 def remove_employee
 end
 
@@ -23,3 +62,7 @@ end
 def change_location
 
 end
+
+
+puts "The program Starts Here"
+menu
