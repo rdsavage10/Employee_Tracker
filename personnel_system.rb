f2bab6ed1fffe9af709a03a12b896fbc94d48fcb
rdsavage10 <rdsavage10@gmail.com>
2019-03-13 08:41:12 -0400

    structure added

--- personnel_system.rb
+++ personnel_system.rb
@@ -1,1 +1,25 @@
 require_relative "employee_class"
+
+def add_employee
+
+Employee.new(name, role, location)
+end
+
+def remove_employee
+end
+
+def location_list
+
+end
+
+# def remove_location
+#
+# end
+
+def employee_list
+
+end
+
+def change_location
+
+end
